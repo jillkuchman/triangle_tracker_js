58c12ab15667785287f101026e08a6094892ac65
James Nielson <james@changemachine.org>
2015-04-10 11:24:28 -0700

    Test 1. Very impressive triangle logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,33 @@
-function triangulize() {
-
-    // var sides = [];
-
-    var sides_input = document.getElementsByClassName( 'sides' ),
-        sides  = [].map.call(sides_input, function( input ) {
-            return input.value;
-    });
-    console.log(sides + 'unsorted');
-    sides.sort();
-    console.log(sides + 'sorted');
+function triangulize(sides) {
+    var sides_array = [sides[0], sides[3], sides[6]]
+    var triangle_type = '';
+    // var sides_input = document.getElementsByClassName( 'sides' ),
+    //     sides  = [].map.call(sides_input, function( input ) {
+    //         return input.value;
+    // });
+    var sides = sides_array.sort();
     var a = sides[0];
     var b = sides[1];
     var c = sides[2];
 
-debugger;
+    if (a + b <= c || a + b >= c) {
+        triangle_type = 'TRIANGLE!';
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+// debugger;
     // var output =
-    return output;
+    return triangle_type;
 }
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,11 @@
 describe('getWordOrder', function() {
     it("returns 'triangle' when given any 3 sides (legit or not)", function() {
-        expect(triangulize('2, 2, 8')).to.equal('triangle');
-    });//watch input, array vs string vs integers
+        expect(triangulize('2, 8, 2')).to.equal('TRIANGLE!');
+    });
 
-    // it("returns 'triangle' when given 3 legit sides", function() {
-    //     expect(triangulize('')).to.equal('triangle');
-    // });
+    it("returns 'triangle' when given 3 legit sides", function() {
+        expect(triangulize('')).to.equal('triangle');
+    });
     //
     // it("returns 'equilateral' when given '3, 3, 3'", function() {
     //     expect(getWordOrder('3, 3, 3')).to.equal(['equilateral']);
