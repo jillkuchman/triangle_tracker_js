e46b06bda477514992923bc78fca7d282253c33e
James Nielson <james@changemachine.org>
2015-04-10 13:19:58 -0700

    Tests & function look swell.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,39 @@
 function triangulize(sides) {
     var triangleType = '';
     sides.sort();
-    var a = sides[0];
-    var b = sides[1];
-    var c = sides[2];
 
-    if (a + b >= c) {
-        triangleType = 'TRIANGLE!';
-    } else return "HAVE YOU EVER SEEN A TRIANGLE!?";
+    var a = parseInt(sides[0]);
+    var b = parseInt(sides[1]);
+    var c = parseInt(sides[2]);
 
+    if (c > a + b) {
+        triangleType = 'INCOMPATIBLE SIDES! TRI AGAIN!';
+    }
+    else if (a === b && a === c) {
+        triangleType = 'EQUILATERAL!';
+    }
+    else if (a === b || a === c || b === c) {
+        triangleType = 'ISOSCELES!';
+    }
+    else  {
+        triangleType = 'SCALENE!';
+    }
 
-
-
-
-
-
-
-
-
-
-// debugger;
     return triangleType;
 }
 
 
 jQuery(document).ready(function() {
     $("#side1").focus();
+    // $("#triangleType").text('');
+
     $("#triangle-sides").submit(function(event) {
-        // var sides = $("#words").val();
+
         var sides_input = document.getElementsByClassName( 'sides' ),
             sides  = [].map.call(sides_input, function( input ) {
                 return input.value;
         });
         var triangleType = triangulize(sides);
-// debugger;
 
         $("#triangleType").text(triangleType);
         $("#result").show();
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,18 @@
 describe('getWordOrder', function() {
-    it("returns 'HAVE YOU EVER SEEN A TRIANGLE!?' when given any 3 sides (legit or not)", function() {
-        expect(triangulize([2, 8, 2])).to.equal('HAVE YOU EVER SEEN A TRIANGLE!?');
+    it("returns 'INCOMPATIBLE SIDES! TRI AGAIN!' when given 3 illegitimate sides)", function() {
+        expect(triangulize([1, 8, 2])).to.equal('INCOMPATIBLE SIDES! TRI AGAIN!');
     });
 
-    it("returns 'TRIANGLE!' only when given 3 legit sides", function() {
-        expect(triangulize([5, 8, 5])).to.equal('TRIANGLE!');
+    it("returns default 'SCALENE!' when given 3 legit sides", function() {
+        expect(triangulize([6, 8, 5])).to.equal('SCALENE!');
     });
 
     it("returns 'EQUILATERAL!' when given 3 matching sides", function() {
-        expect(getWordOrder([3, 3, 3])).to.equal(['EQUILATERAL!']);
+        expect(triangulize([3, 3, 3])).to.equal('EQUILATERAL!');
+    });
+
+    it("returns 'ISOSCELES!' when given 2 matching sides", function() {
+        expect(triangulize([3, 1, 3])).to.equal('ISOSCELES!');
     });
 
 
