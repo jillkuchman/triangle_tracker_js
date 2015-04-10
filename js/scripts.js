58e06eec7bb6d704b6cccd661c7a2e8f319d47de
James Nielson <james@changemachine.org>
2015-04-10 15:21:10 -0700

    Fixed sorting

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
-function triangulize(sides) {
+function triangulize(sides_input) {
     var triangleType = '';
-    sides.sort();
+    var sides = [];
+    for(var i in sides_input) {
+        sides.push(parseInt(sides_input[i]));
+    }
+    console.log(sides);
+    sides.sort(function(a, b){
+        return a-b;
+    });
+    console.log(sides);
 
     var a = parseInt(sides[0]);
     var b = parseInt(sides[1]);
     var c = parseInt(sides[2]);
 
-    if (c > a + b) {
+    if (a + b <= c) {
         triangleType = 'INCOMPATIBLE SIDES! TRI AGAIN!';
     }
     else if (a === b && a === c) {
@@ -18,7 +26,9 @@ function triangulize(sides) {
     else  {
         triangleType = 'SCALENE!';
     }
-
+    if (Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)) {
+        triangleType = triangleType + '(and it is also a right triangle)';
+    }
     return triangleType;
 }
 
