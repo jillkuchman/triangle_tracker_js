a4d8d161385836fd9486c22e09c23dd4fb24a9b9
Jill Kuchman & James Nielson <student@epicodus.com>
2015-04-14 12:29:35 -0700

    Add right triangle distinction and footnote

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,87 @@
-function triangulize(sides_input) {
-    var sides = [];
-    for(var i in sides_input) {
-        sides.push(parseInt(sides_input[i]));
-    }
-
-    sides.sort(function(a, b){
-        return a-b;
-    });
-
-    var input_side_a = parseInt(sides[0]);
-    var input_side_b = parseInt(sides[1]);
-    var input_side_c = parseInt(sides[2]);
-
-    var triangle = {    side_a: input_side_a,
-                        side_b: input_side_b,
-                        side_c: input_side_c,
-                        triangle_type: function() {
-                            if (side_a + side_b <= side_c ) {
-                                type = "invalid";
-                            }
-                            else if (side_a === side_b && side_a === side_c) {
-                                type = "equilateral";
-                            }
-                            else if (side_a === side_b || side_a === side_c || side_b === side_c) {
-                                type = "isosceles";
-                            }
-                            else if (Math.pow(side_a, 2) + Math.pow(side_b, 2) === Math.pow(side_c, 2)) {
-                                type="right";
-                            }
-                            else {
-                                type = "scalene";
-                            }
-                            return type;
-                        }
-
-    }
-
-
 jQuery(document).ready(function() {
     $("#side1").focus();
 
     $("#triangle-sides").submit(function(event) {
 
-        var sides_input = document.getElementsByClassName( 'sides' ),
-            sides  = [].map.call(sides_input, function( input ) {
+        event.preventDefault();
+
+        var sides_input = document.getElementsByClassName( 'sides' );
+        var sides  = [].map.call(sides_input, function( input ) {
                 return input.value;
         });
-        var triangleType = triangulize(sides);
-
-        if(triangleType === 'EQUILATERAL!') {
-            $("ul#equilateral").append("<li>" + sides + "</li>");
-        }
-        else if(triangleType === 'ISOSCELES!') {
-            $("ul#isosceles").append("<li>" + sides + "</li>");
-        }
-        else if(triangleType === 'SCALENE!') {
-            $("ul#scalene").append("<li>" + sides + "</li>");
+        var sides_int = [];
+        for(var i in sides) {
+            sides_int.push(parseInt(sides[i]));
         }
 
-        // $("#triangleType").text(triangleType);
-        // $("#result").show();
-        event.preventDefault();
+        sides_int.sort(function(a, b){
+            return a-b;
+        });
+        console.log(sides_int);
+
+        var input_side_a = parseInt(sides_int[0]);
+        var input_side_b = parseInt(sides_int[1]);
+        var input_side_c = parseInt(sides_int[2]);
+        var is_right = false;
+        var asterisk = "-";
+        console.log(input_side_a);
+
+
+        var new_triangle = {    side_a: input_side_a,
+                                side_b: input_side_b,
+                                side_c: input_side_c,
+                                right_status: asterisk,
+                                triangleType: function() {
+                                    if (this.side_a + this.side_b <= this.side_c || this.side_a === 0) {
+                                        type = "invalid";
+                                    }
+                                    else if (this.side_a === this.side_b && this.side_a === this.side_c) {
+                                        type = "equilateral";
+                                    }
+                                    else if (this.side_a === this.side_b || this.side_a === this.side_c || this.side_b === this.side_c) {
+                                        type = "isosceles";
+                                    }
+                                    else {
+                                        type = "scalene";
+                                    }
+                                    if (Math.pow(this.side_a, 2) + Math.pow(this.side_b, 2) === Math.pow(this.side_c, 2)) {
+                                        type += " right";
+                                    }
+
+                                    return type;
+                                },
+                                // isRight: function() {
+                                //     if (is_right) {
+                                //         asterisk = "*";
+                                //     }
+                                //     return asterisk;
+                                // },
+
+                            };
+
+                            console.log(asterisk);
+
+        // var triangleType = triangulize(sides);
+
+        if(new_triangle.triangleType() === 'equilateral') {
+            $("ul#equilateral").append("<li>" + input_side_a + ", " + input_side_b + ", " + input_side_c + "</li>");
+        }
+        else if(new_triangle.triangleType() === 'isosceles') {
+            $("ul#isosceles").append("<li>" + input_side_a + ", " + input_side_b + ", " + input_side_c + "</li>");
+        }
+        else if(new_triangle.triangleType() === 'scalene') {
+            $("ul#scalene").append("<li>" + input_side_a + ", " + input_side_b + ", " + input_side_c + "</li>");
+        }
+        else if(new_triangle.triangleType() === 'isosceles right') {
+            $("ul#isosceles").append("<li>" + input_side_a + ", " + input_side_b + ", " + input_side_c + "* </li>");
+        }
+        else if(new_triangle.triangleType() === 'scalene right') {
+            $("ul#scalene").append("<li>" + input_side_a + ", " + input_side_b + ", " + input_side_c + "* </li>");
+        }
+
+        else if(new_triangle.triangleType() === 'invalid') {
+            alert("Do it right, asshole.");
+        }
+
     });
 });
